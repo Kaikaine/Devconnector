9bf36133d8a9bff18dd6bfe9690dcc8426ea9ace
kaikaine <kairijb@protonmail.com>
2018-12-03 14:17:46 -0800

    added comment and delete comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -118,33 +118,88 @@ router.post(
 // desc     Unlike post
 // access   private
 router.post(
-    "/unlike/:id",
-    passport.authenticate("jwt", { session: false }),
-    (req, res) => {
-      Profile.findOne({ user: req.user.id }).then(profile => {
-        Post.findById(req.params.id).then(post => {
-          if (
-            post.likes.filter(like => like.user.toString() === req.user.id)
-              .length === 0
-          ) {
-            return res
-              .status(400)
-              .json({ notliked: "You have not yet liked this post" });
-          }
-  
-          // Get remove index
-          const removeIndex = post.likes
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length === 0
+        ) {
+          return res
+            .status(400)
+            .json({ notliked: "You have not yet liked this post" });
+        }
+
+        // Get remove index
+        const removeIndex = post.likes
           .map(item => item.user.toString())
-          .indexOf(req.user.id)
+          .indexOf(req.user.id);
 
         //  splice out of array
-        post.likes.splice(removeIndex, 1)
+        post.likes.splice(removeIndex, 1);
+
+        // save
+        post.save().then(post => res.json(post));
+      });
+    });
+  }
+);
+
+// route    POST api/comment/:id
+// desc     Comment on a post
+// access   private
+router.post('/comment/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return an errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+    .then(post => {
+        const newComment = {
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        }
+
+        // Add to comments array
+        post.comments.unshift(newComment)
 
         // save
         post.save().then(post => res.json(post))
-        });
-      });
-    }
-  );
+    })
+    .catch(err => res.status(404).json({postnotfound: 'No post found'}))
+})
+
+// route    DELETE api/comment/:id/:comment_id
+// desc     Remove comment from post
+// access   private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+    Post.findById(req.params.id)
+    .then(post => {
+        // check if comment exists
+        if(post.comments.filter(comments => comments._id.toString() === req.params.comment_id.length === 0)) {
+            return res.status(404).json({commentnotfound: 'Comment does not exist'})
+        }
+
+        // get remove index
+        const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id)
+
+        // splice comment out of array
+        post.comments.splice(removeIndex, 1)
+
+        post.save().then(post => res.json(post))
+    })
+    .catch(err => res.status(404).json({postnotfound: 'No post found'}))
+})
 
 module.exports = router;
