d27c56df75aa21deefcc79c8342a159bcba6839c
kaikaine <kairijb@protonmail.com>
2018-12-12 14:54:40 -0800

    profiles now display all user information

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
-import connect from './../profiles/Profiles';
+import React, { Component } from "react";
+import isEmpty from "./../../validation/is-empty";
 
 class ProfileAbout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( <div>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { profile } = this.props;
 
+    // get first name
+    const firstname = profile.user.name.trim().split(" ")[0];
 
-        </div> );
-    }
+    // skill list
+    const skills = profile.skills.map((skill, i) => (
+      <div key={i} className="p-3">
+        <i className="fa fa-check" />
+        {skill}
+      </div>
+    ));
+
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{firstname}'s Bio</h3>
+            <p className="lead">
+              {isEmpty(
+                profile.bio ? (
+                  <span>{firstname} does not have a bio</span>
+                ) : (
+                  <span>{profile.bio}</span>
+                )
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default ProfileAbout;
