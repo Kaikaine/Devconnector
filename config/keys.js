6d7f73c3a53cce0e02a27925853c048c5834555b
kaikaine <kairijb@protonmail.com>
2018-12-13 15:01:52 -0800

    prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb://kairi:kairi123@ds029801.mlab.com:29801/kairidev',
-    secret: 'secret'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
+
 }
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://kairi:kairi123@ds029801.mlab.com:29801/kairidev',
+    secret: 'secret'
+}
