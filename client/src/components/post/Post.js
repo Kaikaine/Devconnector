56a1a5ff4c242809374089372c626a80fd5564af
kaikaine <kairijb@protonmail.com>
2018-12-13 14:18:37 -0800

    users can comment on posts and delete their comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,8 @@ import { getPost } from "./../../actions/postActions";
 import PostItem from "./../posts/PostItem";
 import { Link } from 'react-router-dom';
 import Spinner from './../common/Spinner';
+import CommentForm from './CommentForm';
+import CommentFeed from "./CommentFeed";
 
 class Post extends Component {
   constructor(props) {
@@ -17,7 +19,7 @@ class Post extends Component {
   }
 
   render() {
-    const { post, loading } = this.props.post;
+    const { post, loading, comments } = this.props.post;
     let postContent;
 
     if (post === null || loading || Object.keys(post).length === 0) {
@@ -26,6 +28,8 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
