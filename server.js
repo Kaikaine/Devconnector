eac8af1f9626a9daaf4cb7baa91a0e03a125a86e
kaikaine <kairijb@protonmail.com>
2018-12-13 15:10:50 -0800

    maybe fixed deployment issue

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ if(process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+    console.log("App is running on port " + port);
+});
