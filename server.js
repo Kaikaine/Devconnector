63c7b2ffaca2c3cc93a46ee0dbf8f0362833cb37
kaikaine <kairijb@protonmail.com>
2019-02-11 14:04:21 -0600

    cleaned up server file

--- server.js
+++ server.js
@@ -1,59 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
-
-const app = express()
-// DB Config
+const app = express();
 
 // Body parser middleware
-app.options('*', cors())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const db = require('./config/keys').mongoURI
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-// connect to mongoose
-mongoose.connect(db, { useNewUrlParser: true })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err))
-
-// app.get('/', (req,res) => (
-//     res.send('Hello')
-// ))
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-// Passport config
-require('./config/passport.js')
+// Passport Config
+require('./config/passport')(passport);
 
-// USE routes
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-// Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    // set static folder
-    // app.use(express.static('client/build'))
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
-    app.get('/', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-    
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log("App is running on port " + port);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
