3a878ea39be7fd85e88368a0b9d94f4007405e1b
kaikaine <kairijb@protonmail.com>
2019-02-11 13:43:42 -0600

    another try

--- server.js
+++ server.js
@@ -43,15 +43,11 @@ app.use('/api/posts', posts)
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
     // set static folder
-    // app.use(express.static('client/build'))
-    // app.get('/', (req,res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    // })
-    app.get("/", autoRedirect, function(req, res){
-        res.sendFile(path.resolve(__dirname, "client", 'build', "index.html"));
-     });
-   //Public files <this needs to stay right below app.get("/")!!!!
-     app.use(express.static(__dirname + "/client"))
+    app.use(express.static('client/build'))
+    app.get('/', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+    
 }
 
 
